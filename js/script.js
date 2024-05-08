2939e95ea51f64da9772fb24243f34441556b7fd
Lucas Tadeu Monteiro Guedes Fernandes Salomao <lucas.salomao@sp.senai.br>
2024-05-08 18:04:22 -0300

    update

--- js/script.js
+++ js/script.js
@@ -2,17 +2,19 @@ const btnGerar = document.getElementById('btnGerar');
 const personaInput = document.getElementById('persona');
 const desejoInput = document.getElementById('desejo');
 const txtOutput = document.getElementById('txtoutput');
+const target = document.getElementById('spinner-container');
+var markdown = document.getElementById('markdown');
 
 var opts = {
     lines: 13, // O número de linhas para desenhar
     length: 38, // O comprimento de cada linha
     width: 17, // A largura da linha
-    radius: 45, // O raio do círculo interno
+    radius: 50, // O raio do círculo interno
     scale: 1, // Fator de escala
     corners: 1, // Arredondamento dos cantos (0..1)
     color: '#b91d32', // Cor da linha #rgb ou #rrggbb ou array de cores
     fadeColor: 'transparent', // Cor do desbotamento das linhas
-    speed: 1, // Rounds por segundo
+    speed: 2, // Rounds por segundo
     rotate: 0, // A rotação inicial
     animation: 'spinner-line-fade-quick', // A animação a ser usada
     direction: 1, // 1: horário, -1: anti-horário
@@ -24,25 +26,22 @@ var opts = {
     position: 'absolute' // Elemento de posicionamento
 };
 
-const apiUrl = 'https://msepapi-iz4crpefma-uc.a.run.app:443/generate'; // Substitua com a URL da sua API
+const apiUrl = 'https://msepapi-jkwtayswda-uc.a.run.app/generate'; // Substitua com a URL da sua API
 
 btnGerar.addEventListener('click', async () => {
-    const target = document.getElementById('spinner-container');
-    if (!target) {
-        console.error('Elemento target não encontrado!');
-    } else {
-        var spinner = new Spin.Spinner(opts).spin(target);
-    }
+    
     const persona = personaInput.value;
     const desejo = desejoInput.value;
+    var spinner = new Spin.Spinner(opts);
 
     const data = {
-        uri: "gs://dada-ged/Book MSEP Digital.pdf", // URI fixo
+        uri: "gs://documentos-ged/Book MSEP Digital.pdf", // URI fixo
         text1_1: desejo,
         textsi_1: persona
     };
 
     try {
+        await spinner.spin(target);
         const response = await fetch(apiUrl, {
             method: 'POST',
             headers: {
@@ -51,15 +50,20 @@ btnGerar.addEventListener('click', async () => {
             body: JSON.stringify(data)
         });
 
+        spinner.stop(target); // Pára o spinner após a resposta
+
         if (!response.ok) {
             throw new Error('Erro na requisição da API');
         }
-
         const responseData = await response.json();
-        txtOutput.value = responseData.resultado;
+        txt_markdown = responseData.text;
+        var converter = new showdown.Converter();
+        html = converter.makeHtml(txt_markdown);
+        markdown.innerHTML = html;
+        console.log(markdown);
+
     } catch (error) {
         console.error('Erro:', error);
         txtOutput.value = 'Ocorreu um erro ao processar a solicitação.';
     }
-    spinner.stop(); // Pára o spinner após a resposta
 });
